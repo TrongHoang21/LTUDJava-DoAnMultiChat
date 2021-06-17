68fcd5c844d824ea49500878ea880e9a268e14d2
TrongHoang21 <drawk0129@gmail.com>
2021-06-17 22:39:44 +0700

    Finish Layout Swing for client

---
+++ src/GUI/ClientOnlineList.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.*;
+
+public class ClientOnlineList {
+    private JList listOnlineList;
+    private JPanel panel1;
+    private JButton btnUpdateList;
+    private JLabel labelList;
+    private JLabel labelThisClient;
+}
---
+++ src/GUI/ClientPrivateRoom.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.*;
+
+public class ClientPrivateRoom {
+    private JTextArea listChatLog;
+    private JButton btnBack;
+    private JButton btnSend;
+    private JTextArea boxMsg;
+    private JLabel labelDestination;
+}
